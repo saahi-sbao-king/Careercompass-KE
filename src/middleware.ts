03f67c887708e1eb518f264a39e4c00634f61587
Sadiq <sadiq14526@gmail.com>
2026-06-12 16:10:05 +0000

    import { clerkMiddleware } from '@clerk/nextjs/server';
    export default

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,13 @@
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-// Define which routes are accessible without authentication
-const isPublicRoute = createRouteMatcher([
-  '/', 
-  '/faqs', 
-  '/hub(.*)', 
-  '/auth/login(.*)', 
-  '/auth/signup(.*)',
-  '/api/(.*)'
-]);
-
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|track)).*)',
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for Clerk's auto-proxy path
+    '/__clerk/:path*',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
